58d778013ee54894fd869444127154e4723b7f00
amberm <nekosenpai@gmail.com>
2019-03-20 12:35:54 -0600

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = express();
 
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    //res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
